47de158aa7b8ccdb1a2262157610b197fcb9124f
Sergey.Cheh <chehstream@gmail.com>
2018-09-18 08:33:10 +1000

    search file checker doesn't work. Read comments

--- alias_advanced.py
+++ alias_advanced.py
@@ -9,16 +9,24 @@ from os import path as pt   # to awoid namespace linked_saved?
 
 
 
-def check_shell():
-    sy_SHELL = "default=/bin/bash"
+sy_SHELL = "default=/bin/bash"
+
+def check_shell(sy_SHELL):
     # checks what $SHELL use users system
     # default = /bin/bash
     sy_SHELL = os.environ['SHELL']
     print(('User start from %s')% sy_SHELL)
     return sy_SHELL;
 
-def search_history_file():
+def search_history_file(sy_SHELL):
     # search *sh history file
-    pass
+    # create list with full dir to *sh users files
+    # sample at bash=default to test the bar
+    # see os.path to parse the history! at this point bar doesn't work        
+    if sy_SHELL == 1:
+        sh_history = pt.exists('/home/cheh/')
+    else:
+        print('no matches found!')
 
-check_shell();
+check_shell(sy_SHELL);
+search_history_file(sy_SHELL);
